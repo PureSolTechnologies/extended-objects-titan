b2577fbeaf6a76201163d6ec59a4164a9d522bdf
Rick-Rainer Ludwig <ludwig@puresol-technologies.com>
2014-06-22 21:58:25 +0200

    Change Findbugs plugin version and fixed newly found issue.

--- src/main/java/com/puresoltechnologies/xo/titan/api/TitanXOProvider.java
+++ src/main/java/com/puresoltechnologies/xo/titan/api/TitanXOProvider.java
@@ -17,46 +17,46 @@ import com.puresoltechnologies.xo.titan.impl.metadata.TitanRelationMetadata;
  * @author Rick-Rainer Ludwig
  */
 public class TitanXOProvider
-		implements
-		XODatastoreProvider<TitanNodeMetadata, String, TitanRelationMetadata, String> {
+	implements
+	XODatastoreProvider<TitanNodeMetadata, String, TitanRelationMetadata, String> {
 
-	/**
-	 * This constant contains {@value #TITAN_SCHEME_PREFIX} as prefix for all
-	 * URI protocols referencing a Titan store provider.
-	 */
-	private static final String TITAN_SCHEME_PREFIX = "titan-";
+    /**
+     * This constant contains {@value #TITAN_SCHEME_PREFIX} as prefix for all
+     * URI protocols referencing a Titan store provider.
+     */
+    private static final String TITAN_SCHEME_PREFIX = "titan-";
 
-	/**
-	 * This constant contains {@value #TITAN_CASSANDRA_SCHEME} as protocol a
-	 * Titan store provider on Cassandra.
-	 */
-	private static final String TITAN_CASSANDRA_SCHEME = TITAN_SCHEME_PREFIX
-			+ "cassandra";
+    /**
+     * This constant contains {@value #TITAN_CASSANDRA_SCHEME} as protocol a
+     * Titan store provider on Cassandra.
+     */
+    private static final String TITAN_CASSANDRA_SCHEME = TITAN_SCHEME_PREFIX
+	    + "cassandra";
 
-	@Override
-	public Datastore<TitanStoreSession, TitanNodeMetadata, String, TitanRelationMetadata, String> createDatastore(
-			XOUnit xoUnit) {
-		if (xoUnit == null) {
-			throw new IllegalArgumentException("CdoUnit must not be null!");
-		}
-		URI uri = xoUnit.getUri();
-		if (uri == null) {
-			throw new XOException("No URI is specified for the store.");
-		}
-		String scheme = uri.getScheme();
-		if (!scheme.startsWith(scheme)) {
-			throw new XOException("Only URIs starting with '"
-					+ TITAN_SCHEME_PREFIX + "' are supported by this store.");
-		}
-		String host = uri.getHost();
-		int port = uri.getPort();
-		String keyspace = TitanCassandraStore.retrieveKeyspaceFromURI(uri);
-		switch (scheme) {
-		case TITAN_CASSANDRA_SCHEME:
-			return new TitanCassandraStore(host, port, keyspace);
-		default:
-			throw new XOException("Scheme '" + scheme
-					+ "' is not supported by this store.");
-		}
+    @Override
+    public Datastore<TitanStoreSession, TitanNodeMetadata, String, TitanRelationMetadata, String> createDatastore(
+	    XOUnit xoUnit) {
+	if (xoUnit == null) {
+	    throw new IllegalArgumentException("CdoUnit must not be null!");
 	}
+	URI uri = xoUnit.getUri();
+	if (uri == null) {
+	    throw new XOException("No URI is specified for the store.");
+	}
+	String scheme = uri.getScheme();
+	if (!scheme.startsWith(TITAN_SCHEME_PREFIX)) {
+	    throw new XOException("Only URIs starting with '"
+		    + TITAN_SCHEME_PREFIX + "' are supported by this store.");
+	}
+	String host = uri.getHost();
+	int port = uri.getPort();
+	String keyspace = TitanCassandraStore.retrieveKeyspaceFromURI(uri);
+	switch (scheme) {
+	case TITAN_CASSANDRA_SCHEME:
+	    return new TitanCassandraStore(host, port, keyspace);
+	default:
+	    throw new XOException("Scheme '" + scheme
+		    + "' is not supported by this store.");
+	}
+    }
 }
